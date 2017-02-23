385362e1eaaaeef721eb5b63dca1a96b806ddc5b
Wes Young <wes@barely3am.com>
2017-02-23 16:30:19 -0500

    more refactoring

--- centos7/bootstrap.sh
+++ centos7/bootstrap.sh
@@ -9,10 +9,6 @@ yum -y update
 echo 'updating apt-get tree and installing python-pip'
 sudo yum install -y gcc python-pip python-devel git libffi-devel openssl-devel
 
-echo 'installing ansible...'
-sudo pip install 'setuptools>=18.3,<40.0' 'ansible>=2.2.1.0'
-
-echo 'running ansible...'
-ansible-playbook -i "localhost," -c local site.yml -vv
+bash ../ansible.sh
 
 bash ../test.sh
--- ubuntu14/bootstrap.sh
+++ ubuntu14/bootstrap.sh
@@ -8,10 +8,6 @@ echo 'installing the basics'
 sudo apt-get update && apt-get install -y build-essential python-dev python2.7 python-pip python-dev aptitude \
     python-pip libffi-dev libssl-dev sqlite3 software-properties-common
 
-echo 'installing ansible...'
-sudo -H pip install 'setuptools>=11.3,<34.0' 'ansible==2.2.1.0'
-
-echo 'running ansible...'
-ansible-playbook -i "localhost," -c local site.yml -vv
+bash ../ansible.sh
 
 bash ../test.sh
