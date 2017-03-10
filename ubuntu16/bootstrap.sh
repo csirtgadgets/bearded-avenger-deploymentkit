94cdf0a49c6e34a45a25acfe05f09f80aac9fa9c
Wes Young <wes@barely3am.com>
2017-03-10 15:11:57 -0500

    removed debugging

--- ubuntu16/bootstrap.sh
+++ ubuntu16/bootstrap.sh
@@ -2,16 +2,15 @@
 
 set -e
 
-#export CIF_ELASTICSEARCH=$CIF_ELASTICSEARCH
-#export CIF_ANSIBLE_SDIST=$CIF_ANSIBLE_SDIST
-#
-#echo 'installing the basics'
-#sudo apt-get update && apt-get install -y build-essential python-dev python2.7 python-pip python-dev aptitude \
-#    python-pip libffi-dev libssl-dev sqlite3 software-properties-common
-#
-#bash ../ansible.sh
+export CIF_ELASTICSEARCH=$CIF_ELASTICSEARCH
+export CIF_ANSIBLE_SDIST=$CIF_ANSIBLE_SDIST
+
+echo 'installing the basics'
+sudo apt-get update && apt-get install -y build-essential python-dev python2.7 python-pip python-dev aptitude \
+    python-pip libffi-dev libssl-dev sqlite3 software-properties-common
+
+bash ../ansible.sh
 
 if [ "$CIF_BOOTSTRAP_TEST" -eq '1' ]; then
-    echo "YES!"
     bash ../test.sh
 fi
