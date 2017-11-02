d8d02203c45a71025ee858072228a4a48a5b569d
Wes Young <wes@barely3am.com>
2017-11-02 09:00:52 -0400

    removing ubuntu14

--- ansible.sh
+++ ansible.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo 'installing ansible...'
-sudo pip install 'setuptools>=18.3,<34.0' 'ansible>=2.2.1.0'
+sudo pip install 'setuptools>=18.3,<34.0' 'ansible>=2.3,<2.4'
 
 echo 'running ansible...'
 ansible-playbook -i "localhost," -c local site.yml -vv
