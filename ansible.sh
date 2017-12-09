15ba5c4db547752752b9e76beeaa2f5176061e7d
Wes Young <wes@barely3am.com>
2017-12-09 14:04:20 -0500

    cleanup

--- ansible.sh
+++ ansible.sh
@@ -6,7 +6,13 @@ echo 'installing ansible...'
 sudo pip install 'setuptools>=18.3,<34.0' 'ansible>=2.4,<2.5'
 
 echo 'installing roles'
-ansible-galaxy install -r requirements.yml --ignore-errors
+ansible-galaxy install elastic.elasticsearch,5.5.1
+
+# test to see if we've linked this in development
+# install by default in production
+if [ ! -e roles/csirtgadgets.cif ]; then
+  ansible-galaxy install csirtgadgets.cif
+fi
 
 echo 'running ansible...'
 ansible-playbook -i "localhost," -c local site.yml -vv
