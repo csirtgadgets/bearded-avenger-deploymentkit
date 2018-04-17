e84feb18aa795ece2795b1127e4e3b32cf438be9
Wes Young <wes@barely3am.com>
2018-04-17 10:37:38 -0400

    adjusting for rc2

--- ansible.sh
+++ ansible.sh
@@ -11,7 +11,7 @@ ansible-galaxy install elastic.elasticsearch,5.5.1
 # test to see if we've linked this in development
 # install by default in production
 if [ ! -e roles/csirtgadgets.cif ] && [ ! -e ../roles/csirtgadgets.cif ]; then
-  ansible-galaxy install csirtgadgets.cif,0.0.6
+  ansible-galaxy install csirtgadgets.cif,0.0.7
 fi
 
 echo 'running ansible...'
